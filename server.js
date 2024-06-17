839dec92e935226ce4018682b14b1276efe9e295
Yadvendra Shukla <yadvendrashukla919@gmail.com>
2024-06-17 08:35:34 +0530

    did some testing changes

--- server.js
+++ server.js
@@ -223,7 +223,7 @@ function sendEmail(emailInfo) {
 }
 
 app.post("/webhook/emailEvent", (req, res) => {
-  console.log("this is body:  ++  " + req.body);
+  console.log("this is body:  ++  " + JSON.stringify(req.body));
   const recipient = req.body.recipient;
   const event = req.body.event;
 
